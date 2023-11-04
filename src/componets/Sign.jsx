641ec1abb06cec32e6ae2827b8d2164cc2de8709
Manthesh <vmanthesh20@gmail.com>
2023-11-04 18:19:00 +0530

    new chnages for signup page

---
+++ src/componets/Sign.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sign = () => {
+  return (
+    <div>
+      <p>userlogin</p>
+      <input type="text" placeholder='name' />
+      <input type="text" placeholder='password'/>
+    </div>
+  )
+}
+
+export default Sign
